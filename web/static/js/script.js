b6a654e33790a84055f778d9988f492401fd7c17
qhoangf <nqh130901@gmail.com>
2021-07-16 19:51:03 +0700

    Change type between hour and minute

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,6 +1,16 @@
+function openDropdown() {
+    var openBox = document.getElementById("dropdown-menu");
+
+    if (openBox.style.display == "none") {
+        openBox.style.display = "block";
+        } else {
+        openBox.style.display = "none";
+    }
+}
+
 $(document).ready(function(){   
     $("#launchModal").click(function() {
-        $(".modal").addClass("is-active");  
+        $(".modal").addClass("is-active");
     });
 
     $(".modal-close").click(function() {
@@ -8,12 +18,21 @@ $(document).ready(function(){
     });
 });
 
-function openDropdown() {
-    var openBox = document.getElementById("dropdown-menu");
+function switchTime() {
+    var switchHour = document.getElementById("hour");
+    var switchMinute = document.getElementById("minute");
+    var switchHourText = document.getElementById("in-hour");
+    var switchMinuteText = document.getElementById("in-minute");
 
-    if (openBox.style.display == "none") {
-        openBox.style.display = "block";
-      } else {
-        openBox.style.display = "none";
-      }
+    if (switchHour.style.display == "none") {
+        switchHourText.style.display = "block";
+        switchMinuteText.style.display = "none";
+        switchHour.style.display = "block";
+        switchMinute.style.display = "none";
+    } else {
+        switchHourText.style.display = "none";
+        switchMinuteText.style.display = "block";
+        switchHour.style.display = "none";
+        switchMinute.style.display = "block";
+    }
 }
